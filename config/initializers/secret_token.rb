dbe416dbacf20f492b2c767ea285370a52c3d483
Nik Ormseth <nik@ormseth.net>
2014-08-20 13:59:38 -0700

    initial commit, including mods to Gemfile and database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MicroblogRuby::Application.config.secret_key_base = '20e7ef9226a44e2f98c27bc92dca7bfcb1f2edf0285ea25e70948f2a403b005957d1ebe179320fa09a3f73df8e5142414edb9854e2ebca190893632e9e56e036'
